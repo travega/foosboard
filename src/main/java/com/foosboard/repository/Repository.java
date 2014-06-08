994ef5bd46d0b3ef61d86806db5f7c0174f487bf
skrymer <sonni.nielsen@gmail.com>
2014-06-08 16:50:04 +1200

    Added additional packages and a test directory

---
+++ src/main/java/com/foosboard/repository/Repository.java
@@ -0,0 +1,11 @@
+package com.foosboard.repository;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: skrymer
+ * Date: 8/06/14
+ * Time: 4:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Repository {
+}
