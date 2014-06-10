f5f179a578c6b93bafecae6264691acf86feede4
brettyukich <brettyukich@gmail.com>
2014-06-10 19:06:47 +1200

    Use @RestController annotation

--- src/main/java/com/foosboard/controller/MatchController.java
+++ src/main/java/com/foosboard/controller/MatchController.java
@@ -3,40 +3,37 @@ package com.foosboard.controller;
 import com.foosboard.domain.Match;
 import com.foosboard.service.MatchService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class MatchController {
 
     @Autowired
     MatchService matchService;
 
     @RequestMapping(value="/match", method= RequestMethod.POST)
-    public @ResponseBody
-    Match postMatch(@RequestBody Match match){
+    public Match postMatch(@RequestBody Match match){
         return matchService.createMatch(match);
     }
 
     @RequestMapping(value="/match", method = RequestMethod.GET)
-    public @ResponseBody
-    List<Match> getMatchs() {
+    public List<Match> getMatchs() {
         return matchService.retrieveMatches();
     }
 
     @RequestMapping(value="/match/{id}", method= RequestMethod.GET)
-    public @ResponseBody Match getMatch(@PathVariable String id) {
+    public Match getMatch(@PathVariable String id) {
         return matchService.retrieveMatch(id);
     }
 
     @RequestMapping(value="match/{id}", method = RequestMethod.PUT)
-    public @ResponseBody Match putMatch(@PathVariable String id, @RequestBody Match match){
+    public Match putMatch(@PathVariable String id, @RequestBody Match match){
         return matchService.updateMatch(match);
     }
 
--- src/main/java/com/foosboard/controller/PlayerController.java
+++ src/main/java/com/foosboard/controller/PlayerController.java
@@ -3,44 +3,42 @@ package com.foosboard.controller;
 import com.foosboard.domain.Player;
 import com.foosboard.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class PlayerController {
 
     @Autowired
     PlayerService playerService;
 
     @RequestMapping(value="/player", method=RequestMethod.POST)
-    public @ResponseBody Player postPlayer(@RequestBody Player player){
+    public Player postPlayer(@RequestBody Player player){
         return playerService.createPlayer(player);
     }
 
     @RequestMapping(value="/player", method = RequestMethod.GET)
-    public @ResponseBody List<Player> getPlayers() {
+    public List<Player> getPlayers() {
         return playerService.retrievePlayers();
     }
 
     @RequestMapping(value="/player/{id}", method= RequestMethod.GET)
-    public @ResponseBody Player getPlayer(@PathVariable String id) {
+    public Player getPlayer(@PathVariable String id) {
         return playerService.retrievePlayer(id);
     }
 
     @RequestMapping(value="/player/{id}/rating", method=RequestMethod.GET)
-    public @ResponseBody Object getPlayerRating(@PathVariable String id){
+    public Object getPlayerRating(@PathVariable String id){
         return playerService.retrievePlayerRating(id);
     }
 
-    // TODO discuss id in url/body?
     @RequestMapping(value="player/{id}", method = RequestMethod.PUT)
-    public @ResponseBody Player putPlayer(@PathVariable String id, @RequestBody Player player){
+    public Player putPlayer(@PathVariable String id, @RequestBody Player player){
         return playerService.updatePlayer(player);
     }
 
